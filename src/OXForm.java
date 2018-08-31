f5d4360b1ad70d3fb861ca338f85d20d477b3233
Pum58400 <giggleinheart@hotmail.com>
2018-09-01 04:59:14 +0700

    creat gui

--- src/OXForm.java
+++ src/OXForm.java
@@ -1,2 +1,17 @@
+import javax.swing.*;
+
 public class OXForm {
+    private JLabel lblScoreX;
+    private JLabel lblScoreDraw;
+    private JLabel lblScoreO;
+    private JPanel mainPanel;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
 }
