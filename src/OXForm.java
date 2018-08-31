a42a18557699d3bee6b16579ac3275231e8161ef
Pum58400 <giggleinheart@hotmail.com>
2018-09-01 05:53:18 +0700

    input x all gui

--- src/OXForm.java
+++ src/OXForm.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class OXForm {
     private JLabel lblScoreX;
@@ -16,6 +18,144 @@ public class OXForm {
     private JButton button8;
     private JButton button9;
     private JPanel mainPanel;
+    private OX ox;
+    private int col;
+    private  int row;
+    public OXForm() {
+        ox = new  OX();
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                col = 0;
+                row = 0;
+
+                if(ox.put(col,row)){
+                    render();
+                    process();
+                }
+
+            }
+        });
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                col = 1;
+                row = 0;
+
+                if(ox.put(col,row)){
+                    render();
+                    process();
+                }
+
+            }
+        }); button3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                col = 2;
+                row = 0;
+
+                if(ox.put(col,row)){
+                    render();
+                    process();
+                }
+
+            }
+        }); button4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                col = 0;
+                row = 1;
+
+                if(ox.put(col,row)){
+                    render();
+                    process();
+                }
+
+            }
+        }); button5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                col = 1;
+                row = 1;
+
+                if(ox.put(col,row)){
+                    render();
+                    process();
+                }
+
+            }
+        }); button6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                col = 2;
+                row = 1;
+
+                if(ox.put(col,row)){
+                    render();
+                    process();
+                }
+
+            }
+        }); button7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                col = 0;
+                row = 2;
+
+                if(ox.put(col,row)){
+                    render();
+                    process();
+                }
+
+            }
+        }); button8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                col = 1;
+                row = 2;
+
+                if(ox.put(col,row)){
+                    render();
+                    process();
+                }
+
+            }
+        }); button9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                col = 2;
+                row = 2;
+             
+                if(ox.put(col,row)){
+                    render();
+                    process();
+                }
+
+            }
+        });
+    }
+
+    private void process() {
+
+    }
+
+    private void render() {
+    button1.setText(ox.get(0,0));
+    button2.setText(ox.get(1,0));
+    button3.setText(ox.get(2,0));
+    button4.setText(ox.get(0,1));
+    button5.setText(ox.get(1,1));
+    button6.setText(ox.get(2,1));
+    button7.setText(ox.get(0,2));
+    button8.setText(ox.get(1,2));
+    button9.setText(ox.get(2,2));
+    lblScoreX.setText("X :"+ox.getScoreX());
+    lblScoreX.setText("O :"+ox.getScoreO());
+    lblScoreX.setText("Draw :"+ox.getScoreDraw());
+
+
+
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
